56fb5f2ec25f57d8289c80af521ecdd5b5074d8b
wintun-devop <wintun.hlaing@outlook.com>
2026-07-12 22:56:03 +0630

    error

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -1,7 +1,6 @@
 pub use sea_orm_migration::prelude::*;
 
-mod m20220101_000001_create_table;
-mod m20260712_160809_create_tc_user;
+mod m20260712_162236_create_tc_user;
 
 pub struct Migrator;
 
@@ -9,8 +8,7 @@ pub struct Migrator;
 impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
         vec![
-            Box::new(m20220101_000001_create_table::Migration),
-            Box::new(m20260712_160809_create_tc_user::Migration),
+            Box::new(m20260712_162236_create_tc_user::Migration),
         ]
     }
 }
