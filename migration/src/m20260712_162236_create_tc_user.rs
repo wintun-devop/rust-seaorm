23569721c8d8a2dc65c5181ee78c090786210135
wintun-devop <wintun.hlaing@outlook.com>
2026-07-12 22:59:26 +0630

    table migrate

--- migration/src/m20260712_162236_create_tc_user.rs
+++ migration/src/m20260712_162236_create_tc_user.rs
@@ -1,4 +1,4 @@
-use sea_orm_migration::{prelude::*, schema::*};
+use sea_orm_migration::prelude::*;
 
 #[derive(DeriveMigrationName)]
 pub struct Migration;
@@ -6,36 +6,62 @@ pub struct Migration;
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        // Replace the sample below with your own migration scripts
-        todo!();
-
         manager
             .create_table(
                 Table::create()
-                    .table(Post::Table)
+                    .table(TcUser::Table)
                     .if_not_exists()
-                    .col(pk_auto(Post::Id))
-                    .col(string(Post::Title))
-                    .col(string(Post::Text))
+                    .col(ColumnDef::new(TcUser::Id).text().not_null().primary_key())
+                    .col(
+                        ColumnDef::new(TcUser::Username)
+                            .string_len(100)
+                            .not_null()
+                            .unique_key(),
+                    )
+                    .col(
+                        ColumnDef::new(TcUser::Email)
+                            .string()
+                            .not_null()
+                            .unique_key(),
+                    )
+                    .col(ColumnDef::new(TcUser::Password).string().not_null())
+                    .col(ColumnDef::new(TcUser::Role).string_len(50).default("user"))
+                    .col(ColumnDef::new(TcUser::State).string_len(50).default("active"))
+                    .col(
+                        ColumnDef::new(TcUser::CreatedAt)
+                            .timestamp_with_time_zone()
+                            .not_null()
+                            .default(Expr::current_timestamp()),
+                    )
+                    .col(
+                        ColumnDef::new(TcUser::UpdatedAt)
+                            .timestamp_with_time_zone()
+                            .not_null()
+                            .default(Expr::current_timestamp()),
+                    )
                     .to_owned(),
             )
             .await
     }
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        // Replace the sample below with your own migration scripts
-        todo!();
-
         manager
-            .drop_table(Table::drop().table(Post::Table).to_owned())
+            .drop_table(Table::drop().table(TcUser::Table).to_owned())
             .await
     }
 }
 
 #[derive(DeriveIden)]
-enum Post {
+enum TcUser {
     Table,
     Id,
-    Title,
-    Text,
+    Username,
+    Email,
+    Password,
+    Role,
+    State,
+    #[sea_orm(iden = "createdAt")]
+    CreatedAt,
+    #[sea_orm(iden = "updatedAt")]
+    UpdatedAt,
 }
